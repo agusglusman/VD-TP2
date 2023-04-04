c04bf055a67491151dac388136edc5883d530d62
JuanVarvello <118789279+JuanVarvello@users.noreply.github.com>
2023-04-04 17:27:44 -0300

    Update script.js

--- vd_s1_tp2_Varvello_Juan_Y_Agustina_Glusman/ocupacion/script.js
+++ vd_s1_tp2_Varvello_Juan_Y_Agustina_Glusman/ocupacion/script.js
@@ -1,9 +1,8 @@
-
 d3.csv("astronautas.csv", d3.autoType).then((data) => {
   
   let counts = d3.rollup(
     data,
-    v => (v.length)/2, // Contar la cantidad de astronautas en cada grupo
+    v => v.length, // Contar la cantidad de astronautas en cada grupo
     d => d.ocupacion // Agrupar por ocupación
   );
 
@@ -16,6 +15,13 @@ d3.csv("astronautas.csv", d3.autoType).then((data) => {
         x: d => d[0], // Usar la ocupación como eje x
         y: d => d[1], // Usar la cantidad como eje y
         fill: d => d[0], // Usar la ocupación como color
+      }),
+      Plot.text(counts, {
+        x: d => d[0],
+        y: d => d[1] + 2, // Adjust the y-position of the text
+        text: d => d[1], // Use the quantity as the text label
+        fill: d => d[0],
+        align: "center",
       }),
     ],
     grid: true,
